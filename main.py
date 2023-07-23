38c1e27b5242ba298d693b0f420a2357349201ef
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-07-23 21:37:43 +0100

    Añade nuevos assets

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ with open('data/alumnado.csv', encoding='latin-1') as f:
     reader = csv.DictReader(f, delimiter=';')
     data = list(reader)
 
-tengine = TemplateEngine(project_dir=settings.PROJECT_DIR)
-tengine.render('tarjetas.jinja', nombre='Sergio')
+tengine = TemplateEngine(project_dir=settings.PROJECT_DIR, school_year='2023-2024')
+tengine.render('tarjetas.jinja', student=data[0])
